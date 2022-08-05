d31d9edcbb78929ee412f9efb8e99cccf2e394b1
cac-dean <dean.liu@104.com.tw>
2022-08-05 16:29:49 +0800

    update action index

--- action/index.js
+++ action/index.js
@@ -2145,7 +2145,17 @@ const core = __nccwpck_require__(87);
 const token = core.getInput('token');
 const teams = core.getInput('teams'); //teams id
 
-core.info('test ok');
+
+const parsePullRequestId = githubRef => {
+  const result = /refs\/pull\/(\d+)\/merge/g.exec(githubRef);
+  if (!result) throw new Error("Reference not found.");
+  const [, pullRequestId] = result;
+  return pullRequestId;
+};
+
+const pullRequestId = parsePullRequestId(process.env.GITHUB_REF);
+
+core.info('pullRequestId:' + pullRequestId);
 })();
 
 module.exports = __webpack_exports__;
